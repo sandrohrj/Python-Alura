24ccfa3bc2099f3574995f29025d0da495efd6a4
sandrohrj <sandrohrj@gmail.com>
2022-05-11 22:23:20 -0300

    Projetos Python

---
+++ forca.py
@@ -0,0 +1,42 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas." .format)
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    
+    if(erros == 6):
+            print("Você errou")
+    else:
+            print("Você acertou")
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
